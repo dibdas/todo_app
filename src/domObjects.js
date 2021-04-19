a81a2129f795fef5b8a57c0160df8eaf65e65d51
SamArsanios <samson.kibrom@hotmail.com>
2021-04-19 15:22:07 +0300

    Add DOM objects

---
+++ src/domObjects.js
@@ -0,0 +1,24 @@
+const mainContent = document.querySelector('#content');
+const selectProject = document.querySelector('#mySelectProject');
+const addProjectBtn = document.querySelector('#addProjectBtn');
+const toDoBtn = document.querySelector('#toDoBtn');
+
+const projectForm = document.querySelector('#myProjectForm');
+const projectName = document.querySelector('#projectName');
+const createProject = document.querySelector('#createProject');
+const cancelProject = document.querySelector('#cancelProject');
+
+const myTodoForm = document.querySelector('#myTodoForm');
+const todoTitle = document.querySelector('#todoTitle');
+const todoDescription = document.querySelector('#todoDescription');
+const todoDueDate = document.querySelector('#todoDueDate');
+const todoPriority = document.querySelector('#todoPriority');
+
+const editTodo = document.querySelector('#editTodo');
+const submitTodo = document.querySelector('#submitTodo');
+const cancelTodo = document.querySelector('#cancelTodo');
+
+const todoLists = document.createElement('div');
+const todoListDetails = document.createElement('p');
+
+todosContents = todoListDetails.innerHTML;
