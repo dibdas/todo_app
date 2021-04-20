06f89ca55542bf3d72945ca26463f79972879de0
SamArsanios <samson.kibrom@hotmail.com>
2021-04-20 15:41:20 +0300

    Add style,css loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-      contentBase: path.join(__dirname,'dist'),
-      compress: true
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        // test: /\.js$/,
+        // exclude: /node_modules/,
+        // use: ['babel-loader'],
+      },
+    ],
+  }
+  // module: {
+  // rules: [
+  // {
+  //   test: /\.m?js$/,
+  //   exclude: /node_modules/,
+  //   use: {
+  //     loader: "babel-loader",
+  //     options: {
+  //       presets: ['@babel/preset-env']
+  //     }
+  //   }
+  // }
+  //   {
+  //     test: /\.js$/,
+  //     exclude: /node_modules/,
+  //     use: ['babel-loader'],
+  //   }
+  // ]
+  // }
 };
