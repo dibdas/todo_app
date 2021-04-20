64463035a350bb564bb6335b4c1ba07935f646cb
SamArsanios <samson.kibrom@hotmail.com>
2021-04-20 15:45:04 +0300

    Add Event listeners

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-
+import projectObject from './project';
+import todoObject from './todo';
 import {
     mainContent, selectProject, addProjectBtn, toDoBtn, projectForm, projectName, createProject, cancelProject,
     myTodoForm, todoTitle, todoDescription, todoDueDate, todoPriority, editTodo, submitTodo, cancelTodo, errorMsgsAlert, todoLists, todoListDetails, domObjects
-} from './domObjects.js';
-import projectObject from './project';
-import todoObject from './todo';
+} from './domElements';
+// import './styles.css';
+
 
 
 let allProjects = [];
@@ -19,6 +20,14 @@ const selectOption = () => {
     });
 };
 
+const priorityBg = (priority, todoDiv) => {
+    if (priority === 'High') {
+        todoDiv.classList.add('highPriority');
+    } else if (priority === 'Low') {
+        todoDiv.classList.add('lowPriority');
+    }
+};
+
 const displayCurrentProjects = (currentProject) => {
     if (currentProject == '') {
         currentProject = selectProject.value;
@@ -26,18 +35,18 @@ const displayCurrentProjects = (currentProject) => {
 
     allProjects.forEach((project) => {
         if (project.projectName === currentProject) {
-            // domObjects.
+            domObjects.removeMsg();
             todoLists.innerHTML = '';
             if (project.todoList.length > 0) {
                 for (let i = 0; i < project.todoList.length; i += 1) {
                     const todoDiv = document.createElement('div');
-                    // add priotrity here 
-                    todoDiv.classList.add('tododiv');
+                    priorityBg(project.todoList[i].priority, todoDiv);
+                    todoDiv.classList.add('todoDiv');
 
-                    let todoString = `Title: ${project.todoList[i].title}`;
-                    todoString += `Description :${project.todoList[i].description}`;
-                    todoString += `DueDate :${project.todoList[i].dueDate}`;
-                    todoString += `Priority  :${project.todoList[i].priority}`;
+                    let todoString = `Title: ${project.todoList[i].title} </br>`;
+                    todoString += `Description :${project.todoList[i].description} </br>`;
+                    todoString += `DueDate :${project.todoList[i].dueDate} </br>`;
+                    todoString += `Priority  :${project.todoList[i].priority} </br>`;
                     todoDiv.innerHTML = todoString;
 
                     const editBtn = document.createElement('button');
@@ -73,14 +82,14 @@ const getProjects = () => {
     if (!allProjects) {
         allProjects = []
     }
-}
+};
 
 const initialLoad = () => {
+    getProjects();
     selectOption();
     displayCurrentProjects(currentProject);
-    getProjects();
 }
-initialLoad();
+
 
 const createProjectName = (project) => {
     const newProject = projectObject(project);
@@ -127,4 +136,98 @@ const validateTodoInput = (event) => {
     }
 };
 
+const deleteTodo = (target) => {
+    allProjects.forEach((proj) => {
+        if (proj.projectName === currentProject) {
+            proj.todoList.splice(target.value, 1);
+            saveProjects();
+            displayCurrentProjects(currentProject);
+        }
+    });
+};
 
+const showEditTodoForm = (target) => {
+    domObjects.showTodoForm();
+    domObjects.hideSubmitTodo();
+    allProjects.forEach((proj) => {
+        if (proj.projectName === currentProject) {
+            const temp = proj.todoList[target.value];
+            todoTitle.value = temp.title;
+            todoDescription.value = temp.description;
+            todoDueDate.value = temp.dueDate;
+            todoPriority.value = temp.priority;
+            currentTodo = target.value;
+        }
+    });
+};
+
+const editTodoObject = () => {
+    domObjects.showSubmitTodo();
+    domObjects.hideTodoForm();
+    allProjects.forEach((proj) => {
+        if (proj.projectName === currentProject) {
+            // eslint-disable-next-line max-len
+            const editedTodo = todoObject(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value);
+            proj.todoList[currentTodo] = editedTodo;
+            const objIndex = allProjects.findIndex((obj => obj.projectName == currentProject));
+            allProjects[objIndex] = proj;
+            saveProjects();
+            displayCurrentProjects(currentProject);
+        }
+    });
+};
+
+const btnListner = (targetBtn) => {
+    if (targetBtn.classList == 'editBtn') {
+        showEditTodoForm(targetBtn);
+    } else if (targetBtn.classList == 'deleteBtn') {
+        deleteTodo(targetBtn);
+    }
+};
+
+initialLoad();
+// console.log(initialLoad());
+
+addProjectBtn.onclick = () => {
+    domObjects.showProjectForm();
+};
+
+toDoBtn.onclick = () => {
+    domObjects.showTodoForm();
+};
+
+createProject.onclick = (event) => {
+    validateProjectInput(event);
+}
+
+submitTodo.onclick = (event) => {
+    validateTodoInput(event);
+}
+
+cancelProject.onclick = (event) => {
+    event.preventDefault();
+    domObjects.hideProjectForm();
+}
+
+//
+cancelTodo.onclick = (ev) => {
+    ev.preventDefault();
+    domObjects.hideTodoForm();
+    domObjects.showSubmitTodo();
+};
+
+editTodo.onclick = (ev) => {
+    ev.preventDefault();
+    editTodoObject();
+};
+
+
+
+todoLists.addEventListener('click', (e) => {
+    btnListner(e.target);
+});
+
+selectProject.onchange = () => {
+    currentProject = selectProject.value;
+    displayCurrentProjects(currentProject);
+};
