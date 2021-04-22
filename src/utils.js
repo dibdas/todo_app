4ac3c3758d19fae19b4423b6fed1f0c6eea2a0e0
SamArsanios <samson.kibrom@hotmail.com>
2021-04-22 09:44:42 +0300

    Refactor selectoption and priority levels out of index

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import * as domEs from './domElements'
+
+//Populate Select Project Drop Down
+const selectOption = (projects) => {
+    projects.forEach((project) => {
+        const option = document.createElement('option');
+        option.innerHTML = project.projectName;
+        domEs.selectProject.appendChild(option);
+    });
+};
+
+//Set Priority Color
+const priorityBg = (priority, todoDiv) => {
+    if (priority === 'High') {
+        todoDiv.classList.add('highPriority');
+    } else if (priority === 'Low') {
+        todoDiv.classList.add('lowPriority');
+    }
+};
+
+export { selectOption, priorityBg }
