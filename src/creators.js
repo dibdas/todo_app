e06859e73cb30b4c858abf361b8460a13e81ff5e
dibdas <dibyandu08das@gmail.com>
2021-04-22 14:41:40 +0530

    added creator.js

--- src/creators.js
+++ src/creators.js
@@ -1,10 +1,34 @@
+import todoObject from './todo';
+import projectObject from './project';
+import { saveProjects } from './storage'
+import {selectProject, domObjects} from './domElements'
+import { selectOption } from './utils'
+import { allProjects, currentProject, displayCurrentProjects} from './index'
+
+
+//  allProjects = []; 
+//  currentProject = '';
+
+const createProjectName = (project) => {
+    const newProject = projectObject(project);
+    allProjects.push(newProject);
+    saveProjects(allProjects);
+    domObjects.errorMsgsAlert('the project has been created successfully');
+    selectProject.innerHTML = '';
+    selectOption(allProjects);
+    domObjects.hideProjectForm();
+    currentProject = selectProject.value;
+  };
+
 
 const createTodoObject = (title, description, dueDate, priority) => {
+    // let projects
     if (currentProject == '') {
       createProjectName('Default Project');
     }
   
     const newTodo = todoObject(title, description, dueDate, priority);
+    
     allProjects.forEach((project) => {
       if (project.projectName === currentProject) {
         project.todoList.push(newTodo);
@@ -15,5 +39,5 @@ const createTodoObject = (title, description, dueDate, priority) => {
     });
   };
 
-  export { createTodoObject}
+  export { createProjectName, createTodoObject}
   
