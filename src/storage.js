dc6f3e376eb10e18892f4b0b8591f09c0d2312c8
dibdas <dibyandu08das@gmail.com>
2021-04-22 18:32:43 +0530

    modified storage,js

--- src/storage.js
+++ src/storage.js
@@ -1,16 +1,16 @@
 const saveProjects = (projects) => {
-    const str = JSON.stringify(projects);
+  const str = JSON.stringify(projects);
     localStorage.setItem('allProjects', str); // eslint-disable-line
 };
 
 const getProjects = () => {
-    let projects;
+  let projects;
     const str = localStorage.getItem('allProjects'); // eslint-disable-line
-    projects = JSON.parse(str);
-    if (!projects) {
-        projects = [];
-    }
-    return projects
+  projects = JSON.parse(str);
+  if (!projects) {
+    projects = [];
+  }
+  return projects;
 };
 
-export { saveProjects, getProjects }
+export { saveProjects, getProjects };
